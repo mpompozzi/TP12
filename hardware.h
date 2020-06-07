ad14d8421a6c0ab3547214e37a17122ec5352b84
axel <axel@axel-R430-R480>
2020-06-07 13:26:37 -0300

    comienzo de proyecto - God save Megan ;)

---
+++ hardware.h
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   hardware.h
+ * Author: axel
+ *
+ * Created on 7 de junio de 2020, 13:23
+ */
+
+#ifndef HARDWARE_H
+#define HARDWARE_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* HARDWARE_H */
+
